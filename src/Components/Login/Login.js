8e6644d74c9d84b11cba4ba6a5eabcf4501598d3
Ahmad Kayyali <kayyali18@gmail.com>
2018-10-25 14:01:23 -0600

    Make Login a controlled form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
 
-const Login = () =>{
-  return(
-    <div>
-      <section className="login-credentials">
+class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: '',
+      password: '',
+    }
+  }
+
+  handleChange = (event) => {
+    const {value, name} = event.target
+    this.setState({[name]: value})
+  }
+  render() {
+    const { user, password } = this.state
+    return (
+      <form className="login-form" onSubmit={this.handleSubmit}>
         <h2>Login</h2>
-        <input className="user-name input-dimensions" placeholder="user name"/>
-        <input className="email input-dimensions" placeholder="email"/>
-        <Link to="/"><button>Submit</button></Link>
-      </section>
-    </div>
-  )
+        <label>
+          Username
+          <input name='user' placeholder="Username" type="text" value={user} onChange={this.handleChange}/>
+        </label>
+        <label>
+          Password
+          <input name='password' placeholder="Password" type="password" value={password} onChange={this.handleChange}/>
+        </label>
+        <Link to="/">
+          <button>Submit</button>
+        </Link>
+      </form>
+    )
+  }
 }
 
-export default Login;
+export default Login
