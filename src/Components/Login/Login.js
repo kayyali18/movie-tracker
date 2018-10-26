d47d4ccc942963e71c58f0ab71d8046937e1c0d8
Ahmad Kayyali <kayyali18@gmail.com>
2018-10-25 18:54:07 -0600

    Format and clean code, add tab indexes to form to make Login the main form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -17,66 +17,108 @@ class Login extends Component {
       method: 'POST',
       body: JSON.stringify({
         email: 'tman2272@aol.com',
-        password: 'password'
+        password: 'password',
       }),
       headers: {
-        'content-type': 'application/json'
-      }
+        'content-type': 'application/json',
+      },
     })
 
-    console.log (req)
+    console.log(req)
   }
 
   resetForm = () => {
-    this.setState({password: ''})
+    this.setState({ password: '' })
   }
 
-  handleChange = (event) => {
-    const {value, name} = event.target
-    this.setState({[name]: value})
+  handleChange = event => {
+    const { value, name } = event.target
+    this.setState({ [name]: value })
   }
 
-  submitLogin = (e) => {
-    const {loginCheck} = this.props
-    const {user, password} = this.state
+  submitLogin = e => {
+    const { loginCheck } = this.props
+    const { user, password } = this.state
     e.preventDefault()
     // loginCheck(user, password)
     this.resetForm()
   }
-  
+
   render() {
     const { email, username, password } = this.state
     return (
-      <section className='login-main' >
-      <form className="login-form" onSubmit={this.submitLogin} aria-label='Existing user login form'>
-        <h2>Login</h2>
-        <label>
-          E-mail:
-          <input name='email' placeholder="Email" type="email" value={email} onChange={this.handleChange}/>
-        </label>
-        <label>
-          Password:
-          <input name='password' placeholder="Password" type="password" value={password} onChange={this.handleChange}/>
-        </label>
+      <section className="login-main">
+        <form
+          className="login-form"
+          onSubmit={this.submitLogin}
+          aria-label="Existing user login form"
+        >
+          <h2>Login</h2>
+          <label>
+            E-mail:
+            <input
+              tabIndex="0"
+              name="email"
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              tabIndex="0"
+              name="password"
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+          </label>
           <button>Submit</button>
-      </form>
-      <form className="login-new-user" onSubmit={this.submitLogin} aria-label='Create new MovieTracker account'>
-        <h2>Create Account</h2>
-        <label>
-          Name:
-          <input name='username' placeholder="Username" type="text" value={username} onChange={this.handleChange}/>
-        </label>
-        <label>
-          E-mail:
-          <input name='email' placeholder="Email" type="email" value={email} onChange={this.handleChange}/>
-        </label>
-        <label>
-          Password:
-          <input name='password' placeholder="Password" type="password" value={password} onChange={this.handleChange}/>
-        </label>
-          <button>Submit</button>
-      </form>
-
+        </form>
+        <form
+          className="login-new-user"
+          onSubmit={this.submitLogin}
+          aria-label="Create new MovieTracker account"
+        >
+          <h2>Create Account</h2>
+          <label>
+            Name:
+            <input
+              tabIndex="1"
+              name="username"
+              placeholder="Username"
+              type="text"
+              value={username}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            E-mail:
+            <input
+              tabIndex="1"
+              name="email"
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              tabIndex="1"
+              name="password"
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button tabIndex="1">Submit</button>
+        </form>
       </section>
     )
   }
