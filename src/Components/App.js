9d88bd060de0b3e27ba955c91df72f9be4d11390
Ahmad Kayyali <kayyali18@gmail.com>
2018-10-27 17:46:51 -0600

    Succesfully fetch movie Data for latest movies and transform data to suit our needs

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+
 import Login from './Login/Login';
 import Header from './Header/Header';
 import Nav from './Nav/Nav';
 import Movie from './Movie/Movie';
 import MovieContainer from './MovieContainer/MovieContainer';
-import { Route, NavLink } from 'react-router-dom';
+import * as api from '../Helpers/apiCaller'
 import '../styles/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+async componentDidMount() {
+  const nowPlaying = await api.fetchNowPlaying()
+  console.log (nowPlaying)
+}
   render() {
     return (
       <div>
         <Route exact path='/' render={()=>{
           return(
             <div className="App">
-              <Login /> 
+              <MovieContainer /> 
             </div>
           )
         }}
