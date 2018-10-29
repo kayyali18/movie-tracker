2542854b7fc592002edcc23cb985328b27d5ed80
Nick Dambrosio <ndambro303@gmail.com>
2018-10-29 13:51:49 -0600

    put forms inside of a container

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -50,85 +50,87 @@ class Login extends Component {
         <nav>
           <NavLink to="/">Home</NavLink>
         </nav>
-        <form
-          className="login-form"
-          onSubmit={this.submitLogin}
-          aria-label="Existing user login form"
-        >
-          <div className="user-image"></div>
-          <p>Login</p>
-          <h2>Welcome Back</h2>
-          <label className="email-input">
-            <input
-              tabIndex="0"
-              name="email"
-              placeholder="Email"
-              type="email"
-              value={email}
-              onChange={this.handleChange}
-              className="email-text"
-            />
-          </label>
-          <hr />
-          <label>
-            <input
-              tabIndex="0"
-              name="password"
-              placeholder="Password"
-              type="password"
-              value={password}
-              onChange={this.handleChange}
-              className='password-text'
-            />
-          </label>
-          <div className="github-logo" />
-          <button className='submit-button'>Submit</button>
-        </form>
+        <section className='form-container'>
+          <form
+            className="login-form"
+            onSubmit={this.submitLogin}
+            aria-label="Existing user login form"
+          >
+            <div className="user-image"></div>
+            <p>Login</p>
+            <h2>Welcome Back</h2>
+            <label className="email-input">
+              <input
+                tabIndex="0"
+                name="email"
+                placeholder="Email"
+                type="email"
+                value={email}
+                onChange={this.handleChange}
+                className="email-text"
+              />
+            </label>
+            <hr />
+            <label>
+              <input
+                tabIndex="0"
+                name="password"
+                placeholder="Password"
+                type="password"
+                value={password}
+                onChange={this.handleChange}
+                className='password-text'
+              />
+            </label>
+            <div className="github-logo" />
+            <button className='submit-button'>Submit</button>
+          </form>
 
-        <form
-          className="login-new-user"
-          onSubmit={this.submitLogin}
-          aria-label="Create new MovieTracker account"
-        >
-          <h2 className="new-user-h2">Create Account</h2>
-          <label>
-            <input
-              className="new-user-username"
-              tabIndex="1"
-              name="username"
-              placeholder="Username"
-              type="text"
-              value={username}
-              onChange={this.handleChange}
-            />
-          </label>
-          <hr />
-          <label>
-            <input
-              className="new-user-email"
-              tabIndex="2"
-              name="email"
-              placeholder="Email"
-              type="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <hr />
-          <label>
-            <input
-              className="new-user-password"
-              tabIndex="3"
-              name="password"
-              placeholder="Password"
-              type="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-          </label>
-          <hr />
-          <button tabIndex="1">Submit</button>
-        </form>
+          <form
+            className="login-new-user"
+            onSubmit={this.submitLogin}
+            aria-label="Create new MovieTracker account"
+          >
+            <h2 className="new-user-h2">Create Account</h2>
+            <label>
+              <input
+                className="new-user-username"
+                tabIndex="1"
+                name="username"
+                placeholder="Username"
+                type="text"
+                value={username}
+                onChange={this.handleChange}
+              />
+            </label>
+            <hr />
+            <label>
+              <input
+                className="new-user-email"
+                tabIndex="2"
+                name="email"
+                placeholder="Email"
+                type="email"
+                value={email}
+                onChange={this.handleChange}
+              />
+            </label>
+            <hr />
+            <label>
+              <input
+                className="new-user-password"
+                tabIndex="3"
+                name="password"
+                placeholder="Password"
+                type="password"
+                value={password}
+                onChange={this.handleChange}
+              />
+            </label>
+            <hr />
+            <button tabIndex="1">Submit</button>
+          </form>
+        </section>
       </section>
     )
   }
