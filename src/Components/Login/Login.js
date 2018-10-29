d0723ebb3644a65de2b4b9a1354e7dfca4a966fd
Nick Dambrosio <ndambro303@gmail.com>
2018-10-29 15:51:24 -0600

    animate the login, but overflow hidden aint working

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -53,84 +53,85 @@ class Login extends Component {
         </nav> */}
         <div className="user-image"></div>
         <section className='form-container'>
-          <form
-            className="login-form"
-            onSubmit={this.submitLogin}
-            aria-label="Existing user login form"
-          >
-            <p>Login</p>
-            <h2>Welcome Back</h2>
-            <label className="email-input">
-              <input
-                tabIndex="0"
-                name="email"
-                placeholder="Email"
-                type="email"
-                value={email}
-                onChange={this.handleChange}
-                className="email-text"
-              />
-            </label>
-            <hr />
-            <label>
-              <input
-                tabIndex="0"
-                name="password"
-                placeholder="Password"
-                type="password"
-                value={password}
-                onChange={this.handleChange}
-                className='password-text'
-              />
-            </label>
-            <button className='submit-button'>Submit</button>
-            <div class="create-account">Create Account</div>
-          </form>
-
-          <form
-            className="login-new-user"
-            onSubmit={this.submitLogin}
-            aria-label="Create new MovieTracker account"
-          >
-            <h2 className="new-user-h2">Create Account</h2>
-            <label>
-              <input
-                className="new-user-username"
-                tabIndex="1"
-                name="username"
-                placeholder="Username"
-                type="text"
-                value={username}
-                onChange={this.handleChange}
-              />
-            </label>
-            <hr />
-            <label>
-              <input
-                className="new-user-email"
-                tabIndex="2"
-                name="email"
-                placeholder="Email"
-                type="email"
-                value={email}
-                onChange={this.handleChange}
-              />
-            </label>
-            <hr />
-            <label>
-              <input
-                className="new-user-password"
-                tabIndex="3"
-                name="password"
-                placeholder="Password"
-                type="password"
-                value={password}
-                onChange={this.handleChange}
-              />
-            </label>
-            <hr />
-            <button tabIndex="1" className="new-user-submit">Submit</button>
-          </form>
+          <section className={this.state.formState}>
+            <form
+              className="login-form"
+              onSubmit={this.submitLogin}
+              aria-label="Existing user login form"
+            >
+              <p>Login</p>
+              <h2>Welcome Back</h2>
+              <label className="email-input">
+                <input
+                  tabIndex="0"
+                  name="email"
+                  placeholder="Email"
+                  type="email"
+                  value={email}
+                  onChange={this.handleChange}
+                  className="email-text"
+                />
+              </label>
+              <hr />
+              <label>
+                <input
+                  tabIndex="0"
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  onChange={this.handleChange}
+                  className='password-text'
+                />
+              </label>
+              <button className='submit-button'>Submit</button>
+              <div class="create-account" onClick={this.toggleActive}>Create Account</div>
+            </form>
+            <form
+              className="login-new-user"
+              onSubmit={this.submitLogin}
+              aria-label="Create new MovieTracker account"
+            >
+              <h2 className="new-user-h2">Create Account</h2>
+              <label>
+                <input
+                  className="new-user-username"
+                  tabIndex="1"
+                  name="username"
+                  placeholder="Username"
+                  type="text"
+                  value={username}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <hr />
+              <label>
+                <input
+                  className="new-user-email"
+                  tabIndex="2"
+                  name="email"
+                  placeholder="Email"
+                  type="email"
+                  value={email}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <hr />
+              <label>
+                <input
+                  className="new-user-password"
+                  tabIndex="3"
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <hr />
+              <button tabIndex="1" className="new-user-submit">Submit</button>
+            </form>
+          </section>
         </section>
       </section>
     )
