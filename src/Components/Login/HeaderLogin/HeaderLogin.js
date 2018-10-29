9781d34d09ec7db68a1d276270bbca1c0c44a5aa
Nick Dambrosio <ndambro303@gmail.com>
2018-10-29 14:19:50 -0600

    add app header container class

--- src/Components/Login/HeaderLogin/HeaderLogin.js
+++ src/Components/Login/HeaderLogin/HeaderLogin.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeaderLogin = () => {
   return (
-    <div>
+    <div className="app-header-container">
       <div className="app-logo"></div>
       <h1 className="app-title">Now Playing</h1>
       <p>A Simple Movie Tracking App</p>
