af5bfeff29081e30db21dafddb487175edd072a1
Nick Dambrosio <ndambro303@gmail.com>
2018-10-30 07:42:33 -0600

    create nav component

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
-const Nav = () =>{
-  return(
-    <div>Nav</div>
+
+const Nav = () => {
+  return (
+    <div>
+      <h3 className="now-playing-title-text"> now playing </h3>
+      <h3 className='faves'> faves </h3>
+      <h3 className='logout'> <NavLink to="/login">log out</NavLink> </h3>
+    </div>
   )
 }
 
