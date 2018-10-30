a3d049cf9c4f444818d78cdaeaed5a0e4b83eebb
Nick Dambrosio <ndambro303@gmail.com>
2018-10-30 07:43:11 -0600

    wrap login in navlink

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -7,7 +7,7 @@ const Nav = () => {
     <div>
       <h3 className="now-playing-title-text"> now playing </h3>
       <h3 className='faves'> faves </h3>
-      <h3 className='logout'> <NavLink to="/login">log out</NavLink> </h3>
+      <NavLink to="/login"><h3 className='logout'> log out</h3></NavLink>
     </div>
   )
 }
