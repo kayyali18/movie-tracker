217f2e80166962cf602e7a72fde188332cf977d4
Cierra Washington <cierrajwashi@gmail.com>
2018-10-31 07:32:45 -0600

    fixing faves

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import MovieContainer from '../MovieContainer/MovieContainer';
 import FavesContainer from '../FavesContainer/FavesContainer';
 
 const Main = (props) =>{
   return(
     <div className="Main">
-      <Route exact path='/main/favs' props={props} component={FavesContainer} />
+
+      <Route exact path='/favs' render={(props)=>{
+        return <FavesContainer props={props}/>
+      }}
+      />
+
       <Route exact path='/main' props={props} component={MovieContainer} />
 
 
